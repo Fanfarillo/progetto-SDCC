23f0769ddedbd24e8b04ad69f3245017cdeacdaa
Fanfarillo <matteo.fanfarillo99@gmail.com>
2022-09-20 17:50:09 +0200

    Added Flusk

---
+++ code/pages/PagesDef.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="static")
+
+@app.route("/")
+def menu():
+    return render_template("Menu.html")
+
+if __name__ == "__main__":
+    app.run()
