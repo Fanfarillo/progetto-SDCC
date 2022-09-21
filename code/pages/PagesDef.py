efd149ad49e7dec80674646b3f4b8ca3511db4b2
Fanfarillo <matteo.fanfarillo99@gmail.com>
2022-09-21 22:01:23 +0200

    Aggiunto qualcosa a livello di interfaccia grafica

--- code/pages/PagesDef.py
+++ code/pages/PagesDef.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=('GET','POST'))
 def menu():
     return render_template("Menu.html")
+
+@app.route("/accedi", methods=('GET','POST'))
+def accesso():
+    if request.method == 'POST':
+        email = request.form['inputEmail']
+        return redirect("/"+email+"/home")
+
+    return render_template("Accesso.html")
+
+@app.route("/iscriviti", methods=('GET','POST'))
+def iscrizione():
+    if request.method == 'POST':
+        email = request.form['inputEmail']
+        return redirect("/"+email+"/home")
+
+    return render_template("Iscrizione.html")
+
+@app.route("/<string:email>/home", methods=('GET','POST'))
+def home(email):
+    return render_template("Home.html", email=email)
 
 if __name__ == "__main__":
     app.run()
